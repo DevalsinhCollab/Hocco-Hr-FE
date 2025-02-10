15d76bff53e7a1e2a0c80eff6eb78b3c9f1a5ba7
DevalsinhCollab <deval@collabsoftech.com.au>
2025-02-10 13:54:20 +0530

    new login screen

--- src/pages/Auth/LoginScreen.jsx
+++ src/pages/Auth/LoginScreen.jsx
@@ -1,12 +1,15 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { login, googlelogin } from "../../features/authDetailsSlice";
+import { login } from "../../features/authDetailsSlice";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
 import { gapi } from "gapi-script";
 import { LoadingButton } from "@mui/lab";
 import "../../css/Login.css"
+import { IconButton, TextField } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 export default function LoginScreen() {
   const dispatch = useDispatch();
@@ -58,129 +61,142 @@ export default function LoginScreen() {
     gapi.load("client:auth2", start);
   }, []);
 
-  const handleOnSuccessBtn = async (data) => {
-    const googleloginData = await dispatch(googlelogin(data.tokenId));
+  const handleChange = (e) => {
+    const { value, name } = e.target;
 
-    if (!googleloginData.payload.error) {
-      const token = localStorage.setItem(
-        "token",
-        googleloginData.payload.token
-      );
-      navigate("/");
-    }
+    setUser({
+      ...user,
+      [name]: value,
+    });
   };
 
-  const handleOnFailureBtn = (data) => {
-    toast("User not authenticate");
+  const toggleVisibility = () => {
+    setVisible(!visible);
   };
 
   return (
-    <>
-      <div className="loginBox">
-        <div className="loginImgBox">
-          <img src="./Images/graphic3.svg" alt="svg" className="p-4" />
-        </div>
-        <div className="loginExtraBox">
-          <div className="loginData">
-            <div className="">
-              <h2 className="text-center">Login</h2>
+    <main className="d-flex align-items-center min-vh-100 py-3 py-md-0">
+      <div className="container">
+        <div className="card login-card">
+          <div className="row no-gutters m-0">
+            <div className="col-md-6  images">
+              <img
+                src="/Images/Hocco New Logo.gif"
+                alt="login"
+                className="login-card-img"
+                id="img-1"
+              />
+              <img src="/Images/Doc.png" alt="image" id="img-2" />
             </div>
-            <form method="post" onSubmit={LoginHandler}>
-              <div className="loginForm">
-                <div>
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    className="emailInput form-control"
-                    placeholder="Email"
-                    required={true}
-                    onChange={(e) => {
-                      setUser({ ...user, email: e.target.value });
-                    }}
-                  />
-                </div>
-                <div className="mt-3">
-                  <div className="d-flex justify-content-between">
-                    <label>Password</label>
-                    <label className="text-danger">Forgot Password?</label>
-                  </div>
-                  <div className="d-flex">
-                    <input
-                      type={visible ? "text" : "password"}
-                      className="passInput form-control"
-                      placeholder="Password"
-                      required={true}
-                      onChange={(e) => {
-                        setUser({ ...user, password: e.target.value });
+            <div className="col-md-6">
+              <div className="card-body">
+                <p className="login-card-description">Welcome, Sign In</p>
+                <form onSubmit={LoginHandler}>
+                  <div className="form-group mt-5">
+                    <TextField
+                      id="input"
+                      type="text"
+                      name="email"
+                      label="Email"
+                      variant="standard"
+                      placeholder="Enter Email"
+                      value={user?.email}
+                      onChange={handleChange}
+                      fullWidth
+                      InputProps={{
+                        style: {
+                          color: "white",
+                          borderColor: "white",
+                        },
+                      }}
+                      InputLabelProps={{
+                        style: { color: "white" },
                       }}
                     />
-                    <button
-                      className="p-2 show_pass border ms-2"
-                      type="button"
-                      onClick={() => {
-                        setVisible(!visible);
+                  </div>
+                  <div className="form-group mb-1 mt-3">
+                    <TextField
+                      type={visible ? "text" : "password"}
+                      name="password"
+                      id="input"
+                      label="Password"
+                      variant="standard"
+                      placeholder="Enter Password"
+                      value={user?.password}
+                      onChange={handleChange}
+                      fullWidth
+                      InputProps={{
+                        endAdornment: (
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={toggleVisibility}
+                            edge="end"
+                          >
+                            {visible ? (
+                              <VisibilityOffIcon className="text-warning" />
+                            ) : (
+                              <VisibilityIcon className="text-white" />
+                            )}
+                          </IconButton>
+                        ),
+                        style: {
+                          borderColor: "white",
+                          color: "white",
+                        },
                       }}
+                      InputLabelProps={{
+                        style: { color: "white" },
+                      }}
+                    />
+                  </div>
+                  <div className="d-flex align-items-center justify-content-between mt-3">
+                    {/* {authLoading && authLoading ? (
+                      <>
+                        <button className="login-btn" type="button" disabled>
+                          <span
+                            className="spinner-border spinner-border-sm"
+                            role="status"
+                            aria-hidden="true"
+                          ></span>
+                          Loading...
+                        </button>
+                      </>
+                    ) : ( */}
+                    <>
+                      <LoadingButton
+                        id="login"
+                        className="btn btn-block login-btn"
+                        type="submit"
+                        defaultValue="Login"
+                        sx={{ textTransform: "capitalize" }}
+                        loading={loading}
+                      >
+                        Login
+                      </LoadingButton>
+                      {/* <button
+                        id="login"
+                        className="btn btn-block login-btn "
+                        type="submit"
+                        defaultValue="Login"
+                      >
+                        Login
+                      </button> */}
+                    </>
+                    {/* )} */}
+
+                    {/* <Link
+                      to={"/auth/forgotpassword"}
+                      className="forgot-password-link mb-4"
                     >
-                      {visible ? (
-                        <i className="fa-solid fa-eye"></i>
-                      ) : (
-                        <i className="fa-solid fa-eye-slash"></i>
-                      )}
-                    </button>
+                      Forgot password?
+                    </Link> */}
                   </div>
-                </div>
-                <div className="mt-2">
-                  <div className="d-flex gap-1">
-                    <input type="checkbox" />
-                    <span className="text-secondary fs-6">
-                      Remember password ?
-                    </span>
-                  </div>
-                </div>
-                <div className="py-3">
-                  <div className="d-flex justify-content-center align-items-center w-100">
-                    {/* <button
-                      type="submit"
-                      className="btn btn4 me-3"
-                      style={{ width: "70%" }}
-                    >
-                      Login
-                    </button> */}
-                    <LoadingButton
-                      size="small"
-                      type="submit"
-                      className="btn btn4 me-3"
-                      loading={loading}
-                      style={{ width: "70%" }}
-                    >
-                      Login
-                    </LoadingButton>
-                    {/* <GoogleLogin
-                      style={{width: '30%'}}
-                      className="btn rounded btn5"
-                      clientId="1094420422080-6uef7egom8g22i0f8ehoe5m8mtouh38v.apps.googleusercontent.com"
-                      buttonText="Login with Google"
-                      onSuccess={handleOnSuccessBtn}
-                      onFailure={handleOnFailureBtn}
-                      cookiePolicy={"single_host_origin"}
-                    /> */}
-                  </div>
-                </div>
-                <div>
-                  <div className="d-flex justify-content-center align-items-center gap-1 w-100">
-                    <span className="text-secondary fs-6">
-                      Dont have an account?
-                    </span>
-                    <Link to={"/signup"} className="sign fs-6">
-                      SignUp
-                    </Link>
-                  </div>
-                </div>
+                </form>
               </div>
-            </form>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </main>
   );
 }
